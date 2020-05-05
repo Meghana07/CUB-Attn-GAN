1d428742a0a22c798e9e96f2544d6cd1abd68f2a
ammarnasr <ammarnasraza@gmail.com>
2020-05-05 08:33:44 +0200

    Update losses.py

--- theCode/GAN/losses.py
+++ theCode/GAN/losses.py
@@ -206,7 +206,7 @@ def generator_loss(netsD, image_encoder, fake_imgs, real_labels,
             # err_sent = err_sent + s_loss.data[0]
 
             errG_total += w_loss + s_loss
-            logs += 'w_loss: %.2f s_loss: %.2f ' % (w_loss.data[0], s_loss.data[0])
+            logs += 'w_loss: %.2f s_loss: %.2f ' % (w_loss.item(), s_loss.item())
     return errG_total, logs
 
 
