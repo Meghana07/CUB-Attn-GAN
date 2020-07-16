8b21d24f974628523b4475ece568cc77e8c6e589
ammarnasr <ammarnasraza@gmail.com>
2020-07-16 08:23:31 +0200

    Update BertPlusCUB.py

--- theCode/BertPlusCUB.py
+++ theCode/BertPlusCUB.py
@@ -871,23 +871,6 @@ class BERT_ENCODER(nn.Module):
         self.init_trainable_weights()
 
     def define_module(self, model):
-        self.Conv2d_1a_3x3 = model.Conv2d_1a_3x3
-        self.Conv2d_2a_3x3 = model.Conv2d_2a_3x3
-        self.Conv2d_2b_3x3 = model.Conv2d_2b_3x3
-        self.Conv2d_3b_1x1 = model.Conv2d_3b_1x1
-        self.Conv2d_4a_3x3 = model.Conv2d_4a_3x3
-        self.Mixed_5b = model.Mixed_5b
-        self.Mixed_5c = model.Mixed_5c
-        self.Mixed_5d = model.Mixed_5d
-        self.Mixed_6a = model.Mixed_6a
-        self.Mixed_6b = model.Mixed_6b
-        self.Mixed_6c = model.Mixed_6c
-        self.Mixed_6d = model.Mixed_6d
-        self.Mixed_6e = model.Mixed_6e
-        self.Mixed_7a = model.Mixed_7a
-        self.Mixed_7b = model.Mixed_7b
-        self.Mixed_7c = model.Mixed_7c
-
         self.emb_features = conv1x1(768, self.nef)
         self.emb_cnn_code = nn.Linear(2048, self.nef)
 
@@ -896,66 +879,8 @@ class BERT_ENCODER(nn.Module):
         self.emb_features.weight.data.uniform_(-initrange, initrange)
         self.emb_cnn_code.weight.data.uniform_(-initrange, initrange)
 
-    def forward(self, x):
-        features = None
-        # --> fixed-size input: batch x 3 x 299 x 299
-        x = nn.Upsample(size=(299, 299), mode='bilinear')(x)
-        # 299 x 299 x 3
-        x = self.Conv2d_1a_3x3(x)
-        # 149 x 149 x 32
-        x = self.Conv2d_2a_3x3(x)
-        # 147 x 147 x 32
-        x = self.Conv2d_2b_3x3(x)
-        # 147 x 147 x 64
-        x = F.max_pool2d(x, kernel_size=3, stride=2)
-        # 73 x 73 x 64
-        x = self.Conv2d_3b_1x1(x)
-        # 73 x 73 x 80
-        x = self.Conv2d_4a_3x3(x)
-        # 71 x 71 x 192
-
-        x = F.max_pool2d(x, kernel_size=3, stride=2)
-        # 35 x 35 x 192
-        x = self.Mixed_5b(x)
-        # 35 x 35 x 256
-        x = self.Mixed_5c(x)
-        # 35 x 35 x 288
-        x = self.Mixed_5d(x)
-        # 35 x 35 x 288
-
-        x = self.Mixed_6a(x)
-        # 17 x 17 x 768
-        x = self.Mixed_6b(x)
-        # 17 x 17 x 768
-        x = self.Mixed_6c(x)
-        # 17 x 17 x 768
-        x = self.Mixed_6d(x)
-        # 17 x 17 x 768
-        x = self.Mixed_6e(x)
-        # 17 x 17 x 768
-
-        # image region features
-        features = x
-        # 17 x 17 x 768
-
-        x = self.Mixed_7a(x)
-        # 8 x 8 x 1280
-        x = self.Mixed_7b(x)
-        # 8 x 8 x 2048
-        x = self.Mixed_7c(x)
-        # 8 x 8 x 2048
-        x = F.avg_pool2d(x, kernel_size=8)
-        # 1 x 1 x 2048
-        # x = F.dropout(x, training=self.training)
-        # 1 x 1 x 2048
-        x = x.view(x.size(0), -1)
-        # 2048
-
-        # global image features
-        cnn_code = self.emb_cnn_code(x)
-        # 512
-        if features is not None:
-            features = self.emb_features(features)
+    def forward(self,  b_input_ids, b_segments_ids):
+        
         return features, cnn_code
 
 
@@ -1197,11 +1122,7 @@ def train(dataloader, cnn_model, rnn_model, batch_size, labels, optimizer, epoch
         #-------------------------------------------------------------------------------
                 #---------------------------------------------------------
                         #-----------------------------------
-        with torch.no_grad():
-            outputs = model(b_input_ids, b_segments_ids)
-            hidden_states = outputs[2]
-
-        
+        bert_encoder(b_input_ids, b_segments_ids)
                         #-----------------------------------
                 #---------------------------------------------------------
         #-------------------------------------------------------------------------------
